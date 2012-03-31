4d21d4b8ba24c2942db87498014add5f7e7fa683
Nadav Kavalerchik <nadavkav@gmail.com>
2012-04-01 00:34:28 +0300

    MDL-31777 - Switch block's columns when course is set to RTL Mode

--- theme/base_rtl/layout/general.php
+++ theme/base_rtl/layout/general.php
@@ -42,9 +42,19 @@ $hascustommenu = (empty($PAGE->layout_options['nocustommenu']) && !empty($custom
 
 $bodyclasses = array();
 if ($showsidepre && !$showsidepost) {
-    $bodyclasses[] = 'side-pre-only';
+    if (!right_to_left()) {
+        $bodyclasses[] = 'side-pre-only';
+    } else {
+        $bodyclasses[] = 'side-post-only';
+    }
+    //$bodyclasses[] = 'side-pre-only';
 } else if ($showsidepost && !$showsidepre) {
-    $bodyclasses[] = 'side-post-only';
+    if (!right_to_left()) {
+        $bodyclasses[] = 'side-post-only';
+    } else {
+        $bodyclasses[] = 'side-pre-only';
+    }
+    //$bodyclasses[] = 'side-post-only';
 } else if (!$showsidepost && !$showsidepre) {
     $bodyclasses[] = 'content-only';
 }
@@ -112,32 +122,27 @@ echo $OUTPUT->doctype() ?>
                     </div>
                 </div>
 
-                <?php if ($hassidepre) { ?>
+                <?php if ($hassidepre OR (right_to_left() AND $hassidepost)) { ?>
                 <div id="region-pre" class="block-region">
-                   <div class="region-content">
-                          <?php
-                          $sidepreonly = ($showsidepre && !$showsidepost);
-                      if (right_to_left() AND !$sidepreonly) {
-                          echo $OUTPUT->blocks_for_region('side-post');
-                      } else {
-                          echo $OUTPUT->blocks_for_region('side-pre');
-                   } ?>
-
-                   </div>
+                    <div class="region-content">
+                        <?php if (!right_to_left()) {
+                                echo $OUTPUT->blocks_for_region('side-pre');
+                              } elseif ($hassidepost) {
+                                echo $OUTPUT->blocks_for_region('side-post');
+                              } ?>
+                    </div>
                 </div>
                 <?php } ?>
 
-                <?php if ($hassidepost) { ?>
+                <?php if ($hassidepost OR (right_to_left() AND $hassidepre)) { ?>
                 <div id="region-post" class="block-region">
-                   <div class="region-content">
-                          <?php
-                          $sidepostonly = ($showsidepost && !$showsidepre);
-                      if (right_to_left() AND !$sidepostonly) {
-                          echo $OUTPUT->blocks_for_region('side-pre');
-                      } else {
-                          echo $OUTPUT->blocks_for_region('side-post');
-                   } ?>
-                   </div>
+                    <div class="region-content">
+                        <?php if (!right_to_left()) {
+                                echo $OUTPUT->blocks_for_region('side-post');
+                              } elseif ($hassidepre) {
+                                echo $OUTPUT->blocks_for_region('side-pre');
+                              } ?>
+                    </div>
                 </div>
                 <?php } ?>
             </div>
