a63d7990d8b2fc0ced7bab037d8832e6b77eec27
Nadav Kavalerchik <nadavkav@gmail.com>
2012-05-06 15:08:09 +0300

    MDL-32818 - RTL Theme fixes for Moodle 2 (theme/splash)
    Right align header's custom menu, when in RTL mode
    theme/splash/style/menu_dropdown.css
    theme/splash/style/menu_rtl.css
    theme/splash/config.php
    theme/splash/renderers.php
    theme/splash/pix/menu/*
    
    Support for blocks columns switch when in rtl mode:
    theme/splash/layout/report.php
    theme/splash/layout/general.php
    theme/splash/style/core.css

--- theme/splash/layout/general.php
+++ theme/splash/layout/general.php
@@ -37,9 +37,17 @@ splash_initialise_colourswitcher($PAGE);
 $bodyclasses = array();
 $bodyclasses[] = 'splash-'.splash_get_colour();
 if ($hassidepre && !$hassidepost) {
-    $bodyclasses[] = 'side-pre-only';
-} else if ($hassidepost && !$hassidepre) {
-    $bodyclasses[] = 'side-post-only';
+    if (!right_to_left()) {
+        $bodyclasses[] = 'side-pre-only';
+    }else{
+        $bodyclasses[] = 'side-post-only';
+    }
+} else if ($showsidepost && !$showsidepre) {
+    if (!right_to_left()) {
+        $bodyclasses[] = 'side-post-only';
+    }else{
+        $bodyclasses[] = 'side-pre-only';
+    }
 } else if (!$hassidepost && !$hassidepre) {
     $bodyclasses[] = 'content-only';
 }
@@ -171,23 +179,32 @@ echo $OUTPUT->doctype() ?>
                         </div>
                     </div>
 
-                    <?php if ($hassidepre) { ?>
+                    <?php if ($hassidepre OR (right_to_left() AND $hassidepost)) { ?>
                     <div id="region-pre" class="block-region">
                         <div class="region-content">
-                            <?php echo $OUTPUT->blocks_for_region('side-pre') ?>
+                            <?php
+                            if (!right_to_left()) {
+                                echo $OUTPUT->blocks_for_region('side-pre');
+                            } elseif ($hassidepost) {
+                                echo $OUTPUT->blocks_for_region('side-post');
+                            } ?>
+
                         </div>
                     </div>
-                    <?php
-} ?>
+                    <?php } ?>
 
-                    <?php if ($hassidepost) { ?>
+                    <?php if ($hassidepost OR (right_to_left() AND $hassidepre)) { ?>
                     <div id="region-post" class="block-region">
                         <div class="region-content">
-                            <?php echo $OUTPUT->blocks_for_region('side-post') ?>
+                            <?php
+                            if (!right_to_left()) {
+                                echo $OUTPUT->blocks_for_region('side-post');
+                            } elseif ($hassidepre) {
+                                echo $OUTPUT->blocks_for_region('side-pre');
+                            } ?>
                         </div>
                     </div>
-                    <?php
-} ?>
+                    <?php } ?>
                 </div>
             </div>
         </div>
